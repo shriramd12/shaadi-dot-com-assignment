a85c4a4f5b03589857f6062f6cb6deb77a7da6a8
Shriram <shriramd12@gmail.com>
2024-09-02 21:05:23 +0530

    Initial Commit

---
+++ MatchMate/MatchMateApp.swift
@@ -0,0 +1,20 @@
+//
+//  MatchMateApp.swift
+//  MatchMate
+//
+//  Created by Shriram Dharmadhikari on 02/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MatchMateApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
